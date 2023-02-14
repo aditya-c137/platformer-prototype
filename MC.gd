1a78c2f8d866603f756a8df965d024d5ceb5629f
Aditya Borse <aditya2352k@gmail.com>
2023-02-14 18:12:15 +0530

    Movement, animations and bug fixes
    - Implemented Jump Buffer, Double Jump and Coyote Jump
    - Made basic animations and used AnimationTree to choose the right animation
    - Fixed bug where the player would not jump if both left and right keys were kept pressed

--- MC.gd
+++ MC.gd
@@ -3,6 +3,11 @@ extends KinematicBody2D
 
 export(Resource) var player_props = preload("res://DefaultPlayer.tres")
 
+onready var jump_buffer_timer = $JumpBufferTimer
+onready var animation_tree = $AnimationTree
+onready var player_sprite = $Sprite
+onready var coyote_jump_timer = $CoyoteJumpTimer
+
 #calculated player parameters
 var GRAVITY: float
 var JUMP_VELOCITY: float
@@ -10,30 +15,63 @@ var MAX_VELOCITY_X: float
 var ACCELERATION: float
 var DECELERATION: float
 
+#local variables
 var velocity = Vector2.ZERO
+var max_jumps: int = 0
+var buffered_jump: bool = false
+var coyote_jump: bool = false
+var was_on_floor: bool
+var just_left_ground:bool = false
 
 func _ready():
 	calculate_gravity_and_velocity()
+	jump_buffer_timer.wait_time = player_props.JUMP_BUFFER_WAIT_TIME
+	animation_tree.active = true
 
 func _physics_process(delta):
-	apply_gravity(delta)
 	var input = Vector2.ZERO
 	input.x = Input.get_axis("ui_left", "ui_right")
+	
+	apply_gravity(delta)
+	jump_player(delta)
 	move_player(input, delta)
+
+	was_on_floor = is_on_floor()
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if not is_on_floor() and was_on_floor:
+		coyote_jump = true
+		coyote_jump_timer.start()
+
+func jump_player(delta):
+	
+	if is_on_floor() or coyote_jump and velocity.y >=0:
+		max_jumps = player_props.MAX_IN_AIR_JUMPS
+		if Input.is_action_just_pressed("jump") or buffered_jump:
+			velocity.y = -JUMP_VELOCITY
+			buffered_jump = false
+		animation_tree.set("parameters/air_or_ground/current", 0)
+	else:
+		if Input.is_action_just_pressed("jump") and max_jumps > 0:
+			velocity.y = -JUMP_VELOCITY
+			max_jumps -= 1
+		
+		if Input.is_action_just_pressed("jump"):
+			buffered_jump = true
+			jump_buffer_timer.start()
+		animation_tree.set("parameters/air_or_ground/current", 1)
 
 func move_player(input, delta):
 	if input.x != 0:
+		animation_tree.set("parameters/on_ground/current", 1)
+		player_sprite.flip_h = input.x < 0
 		velocity.x = MAX_VELOCITY_X * input.x
 #		apply_acceleration(input.x, delta)         #TODO: advanced horizontal movement
 	else:
+		animation_tree.set("parameters/on_ground/current", 0)
 		velocity.x = 0
 #		apply_friction(delta)                      #TODO: advanced horizontal movement
-	
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = -JUMP_VELOCITY
-	
 
 func apply_gravity(delta):
 	velocity.y += GRAVITY * delta
@@ -53,3 +91,11 @@ func calculate_gravity_and_velocity():
 	print(GRAVITY)
 	print(JUMP_VELOCITY)
 	print(MAX_VELOCITY_X)
+
+
+func _on_JumpBufferTimer_timeout():
+	buffered_jump = false
+
+
+func _on_CoyoteJumpTimer_timeout():
+	coyote_jump = false
--- player_props.gd
+++ player_props.gd
@@ -7,5 +7,5 @@ export(float) var JUMP_TIME
 export(int) var JUMP_RANGE
 export(float) var ACC_TIME
 export(float) var DEC_TIME
-export(int) var MAX_VELOCITY_X
-export(int) var MAX_FALL_VELOCITY
+export(int) var MAX_IN_AIR_JUMPS = 1
+export(float) var JUMP_BUFFER_WAIT_TIME = 0.2
